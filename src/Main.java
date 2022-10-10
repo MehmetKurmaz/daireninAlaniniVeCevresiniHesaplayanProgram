bf6fdad4204e62768fae1a08c0dd6e035290c280
MehmetKurmaz <111184509+MehmetKurmaz@users.noreply.github.com>
2022-10-10 23:47:05 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Dairenin Yari Capini Giriniz : ");
         int r,a;
-        double pi=3.14,alan;
+        double pi=3.14,alan,cevre;
         Scanner input=new Scanner(System.in);
         r=input.nextInt();
         System.out.print("Dairenin Merkez Aci Olcusunu Giriniz : ");
         a= input.nextInt();
         alan=(pi*(r*r)*a)/360;
-        System.out.print("Dairenin Alani :"+alan);
+        cevre=2*pi*r;
 
+        System.out.println("Dairenin Alani :"+alan);
+        System.out.print("Dairenin Cevresi :"+cevre);
     }
 }
